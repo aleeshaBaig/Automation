d45dad611dae8f2e117eb8a904a34a2b907bce36
MacBook <aleeshabaig1@gmail.com>
2023-01-24 10:33:55 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver ;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.quit();
+
     }
 }
+
